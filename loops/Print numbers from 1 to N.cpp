b38d6bc0c7caac98099c47c640ff7cc0480d8501
Laukik  Khake <169252251+laukik08@users.noreply.github.com>
2025-07-22 17:58:46 +0000

    done

---
+++ loops/Print numbers from 1 to N.cpp
@@ -0,0 +1,16 @@
+//1. Print numbers from 1 to N//
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cout<<"enter no.";
+cin>>n;
+    
+for (int i = 1; i<=n; i++)
+{
+    cout<<i<<" ";
+}
+
+    return 0;
+}
