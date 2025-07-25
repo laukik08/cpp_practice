c7037d7780e4228404611cd1bcdee18442e66b37
Laukik  Khake <169252251+laukik08@users.noreply.github.com>
2025-07-25 18:24:46 +0000

    u

--- loops/fibo.cpp
+++ loops/fibo.cpp
@@ -1,22 +1,3 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-    int n, a=0, b=1, c;
-    
-    cout<<"enter no.";
-    cin>>n;
-    
-    if(n<=1) cout<<a<<" ";
-    if(n>=2) cout<<b<<" ";
-    
-    for (int i=3; i<=n; i++)
-    {
-        c=a+b;
-        cout<<c<<" ";
-        a=b;
-        b=c;
-    }
-    return 0;
-}
